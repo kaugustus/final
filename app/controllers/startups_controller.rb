2bdcff24a69342ce5b9e1fff3a7983ccb5354385
vagrant <vagrant@kellogg-dev-box>
2014-06-03 00:37:15 +0000

    startup edit

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -8,4 +8,16 @@ class StartupsController < ApplicationController
 
     redirect_to "/startups"
   end
+  
+  def destroy
+    startup = Startup.find_by("id" => params["id"])
+                startup.delete
+                redirect_to "/startups"
+  end
+
+  def update
+    startup = Startup.find_by("id" => params["id"])
+              startup.update("name" => params["name"], "founding" => params["founding"], "description" => params["description"], "competition_id" => params["chosen_competition_id"])
+              redirect_to "/startups"
+  end
 end
